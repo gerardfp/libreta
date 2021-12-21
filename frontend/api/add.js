6004e352d4f5ba98470595a6b5a00a351499c9cf
gerardfp <gerardfp@gmail.com>
2021-12-21 14:35:51 +0100

    .

--- frontend/api/add.js
+++ frontend/api/add.js
@@ -1,7 +1,7 @@
 import { PSDB } from 'planetscale-node';
 const conni = new PSDB('main')
   
-export default function handler(req, res) {
+export default async function handler(req, res) {
     const [rowsi, fields] = await conni.query('select * from todos')
-    res.send("ok");
+    res.send(rowsi);
 }
