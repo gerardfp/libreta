253d5beb7d81c48a4854473161ad8ac07eb3b8a0
gerardfp <gerardfp@gmail.com>
2021-12-21 15:18:05 +0100

    .

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -22,7 +22,7 @@
 		console.log(process.env);
 		console.log(import.meta.env);
 		// console.log(VITE_VERCEL_URL);
-		return { props: { tareas: (await (await fetch(`${import.meta.env.VITE_VERCEL_URL}/api/`)).json()) }};
+		return { props: { tareas: (await (await fetch(`${process.env.VITE_VERCEL_URL}/api/`)).json()) }};
 		// return { props: { tareas: (await (await fetch(`${import.meta.env.VITE_INTERNAL_API_ENDPOINT}`)).json()) }};
 	}
 </script>
