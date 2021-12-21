bca2acc1e0aad6a7f6ac083b5cdcb28d12f9dbf3
gerardfp <gerardfp@gmail.com>
2021-12-21 14:33:38 +0100

    .

--- frontend/api/add.js
+++ frontend/api/add.js
@@ -2,6 +2,6 @@ import { PSDB } from 'planetscale-node';
 const conni = new PSDB('main')
   
 export default function handler(req, res) {
-    const [rowsi, fields] = await conni.query('select * from reminders')
-    res.send(JSON.stringify(rowsi));
+    const [rowsi, fields] = await conni.query('select * from todos')
+    res.send("ok");
 }
--- frontend/api/index.js
+++ frontend/api/index.js
@@ -2,6 +2,7 @@ import { PSDB } from 'planetscale-node';
 const conni = new PSDB('main')
   
 export default function handler(req, res) {
-    const [rowsi, fields] = await conni.query('select * from reminders')
-    res.send(JSON.stringify(rowsi));
+    //const [rowsi, fields] = await conni.query('select * from todos')
+    //res.send(JSON.stringify(rowsi));
+    res.send("ok")
 }
