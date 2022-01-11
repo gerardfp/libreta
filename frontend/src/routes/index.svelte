937502bbf19d55d8129e6888b6cc8110d984e0d8
gerardfp <gerardfp@gmail.com>
2022-01-11 12:45:23 +0100

    .

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -19,6 +19,7 @@
 
 <script context="module">
 	export async function load({ fetch }) {
+		console.log("loading...");
 		//return { props: { tareas: (await (await fetch(`https://${process.env.VERCEL_URL}/api/`)).json()) }};
 		return { props: { tareas: (await (await fetch(`/api/`)).json()) }};
 	}
